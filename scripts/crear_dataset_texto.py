744cee5f738a469894d17485a987cab606a6bf46
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-11-03 18:17:11 -0600

    Lexico e indices

--- scripts/crear_dataset_texto.py
+++ scripts/crear_dataset_texto.py
@@ -38,16 +38,35 @@ def get_oracle_text(edh):
     return textos
 
 
-mana_cost = "{[1-9WUBRG/P]*}"
+mana_cost = "{[0-9WUBRG/PXC]*}"
 tap_cost = "{T}"
 
 oracle_text = get_oracle_text(edh)
-oracle_text = [re.sub(mana_cost, "mana_cost ", i) for i in oracle_text]
-oracle_text = [re.sub(tap_cost, "tap_cost ", i) for i in oracle_text]
+oracle_text = [re.sub(mana_cost, " mana_cost ", i) for i in oracle_text]
+oracle_text = [re.sub(tap_cost, " tap_cost ", i) for i in oracle_text]
 oracle_text = [i.lower() for i in oracle_text]
-oracle_text = [re.sub("[.,:()]|(â€”)", " ", i).split() for i in oracle_text]
+oracle_text = [re.sub('[.,:()"]|(â€”)', " ", i).split() for i in oracle_text]
 
 names = [i["name"] for i in commanders_json]
 
+texto_cartas = dict(zip(names, oracle_text))
 
-texto_cartas = pd.DataFrame(list(zip(names, oracle_text)), columns=["name", "oracle_text"])
+lexico = []
+
+for nombre, texto in texto_cartas.items():
+    for palabra in texto:
+        if palabra not in lexico:
+            lexico.append(palabra)
+
+lexico.sort()
+len(lexico)
+
+texto_indices = []
+
+for texto in texto_cartas.values():
+    indices = []
+    for palabra in texto:
+        indices.append(lexico.index(palabra))
+    texto_indices.append(indices)
+
+
